4d2c517f6595f9608eecc959c6bc28df76cb24bb
Kirill Firsov <kifirigor@gmail.com>
2018-07-01 13:07:04 +0300

    Add files via upload

---
+++ lesson8/src/main/java/ru/firsov/chat/BaseAuthService.java
@@ -0,0 +1,70 @@
+package ru.firsov.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseAuthService implements AuthService {
+    private class Entry {
+        private String login;
+        private String password;
+        private String nick;
+
+        Entry(String login, String password, String nick) {
+            this.login = login;
+            this.password = password;
+            this.nick = nick;
+        }
+
+        String getLogin() {
+            return login;
+        }
+
+        String getPassword() {
+            return password;
+        }
+
+        String getNick() {
+            return nick;
+        }
+    }
+
+    private List<Entry> entries;
+
+    BaseAuthService() {
+        init();
+    }
+
+    private void init() {
+        entries = new ArrayList<>();
+        entries.add(new Entry("login1", "pass1", "nick1"));
+        entries.add(new Entry("login2", "pass2", "nick2"));
+        entries.add(new Entry("login3", "pass3", "nick3"));
+        entries.size();
+    }
+
+    @Override
+    public String getNick(String login, String pass) {
+        for (Entry e : entries) {
+            if (e.getLogin().equals(login) && e.getPassword().equals(pass)) return e.getNick();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean login(String login, String pass) {
+        for (Entry e : entries) {
+            if (e.getLogin().equals(login) && e.getPassword().equals(pass)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(String userName) {
+        if (userName == null || userName.trim().isEmpty()) return false;
+
+        for (Entry e : entries) {
+            if (userName.equals(e.getNick())) return true;
+        }
+        return false;
+    }
+}
---
+++ lesson8/src/main/java/ru/firsov/chat/MyServer.java
@@ -0,0 +1,64 @@
+package ru.firsov.chat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MyServer {
+    private final List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<>());
+    private AuthService authService;
+
+    public static void main(String[] args) {
+        new MyServer(new BaseAuthService());
+    }
+
+    private MyServer(AuthService authService) {
+        this.authService = authService;
+
+        Socket s = null;
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Server created. Waiting for client...");
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                s = server.accept();
+                ClientHandler client = new ClientHandler(this, s);
+                client.start();
+                clients.add(client);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (server != null) server.close();
+                System.out.println("Server closed");
+                if (s != null) s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    AuthService getAuthService() {
+        return authService;
+    }
+
+    void sendBroadcastMessage(String msg) {
+        for (ClientHandler c : clients) {
+            if (c.isActive()) c.sendMessage(msg);
+        }
+    }
+
+    void sendPrivateMessage(String from, String userName, String message) {
+        for (ClientHandler c : clients) {
+            String name = c.getHandlerName();
+            if (name.equals(userName) && c.isActive())
+                c.sendMessage(from + " написал лично " + userName + ": " + message);
+        }
+    }
+}
